cadcb86ad9d681fd6b9ba028fd559450a486bd19
Nicholas Gustafson <nichgus7@gmail.com>
2020-02-05 15:16:59 -0500

    Adding GZIP compression

--- src/state/client.js
+++ src/state/client.js
@@ -3,7 +3,8 @@ import ApolloClient from 'apollo-boost';
 const client = new ApolloClient({
   uri: 'https://driver-incentive.herokuapp.com/v1/graphql',
   headers: {
-    'x-hasura-admin-secret': process.env.REACT_APP_DATABASE_AUTH_TOKEN
+    'x-hasura-admin-secret': process.env.REACT_APP_DATABASE_AUTH_TOKEN,
+    'Accept-Encoding': 'gzip'
   }
 });
 
