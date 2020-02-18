445639df45f9378ed70042646d44a7109b3f40d7
Nicholas Gustafson <nichgus7@gmail.com>
2020-02-18 15:50:14 -0500

    Getting rid of the footer so we can figure it out later

--- src/pages/register/index.js
+++ src/pages/register/index.js
@@ -3,43 +3,39 @@ import Card from 'react-bootstrap/Card';
 import Nav from 'react-bootstrap/Nav';
 import NewDriverForm from '../../components/new-driver-form';
 import NewSponsorForm from '../../components/new-sponsor-form';
-import Footer from '../../components/footer';
 import './register.scss';
 
 const Register = () => {
   const [registerSheet, setRegisterSheet] = useState('driver');
 
   return (
-    <>
-      <div className="register">
-        <h1 className="register__heading">Register</h1>
-        <Card>
-          <Card.Header>
-            <Nav fill variant="tabs" defaultActiveKey="#driver">
-              <Nav.Item>
-                <Nav.Link
-                  href="#driver"
-                  onClick={() => setRegisterSheet('driver')}>
-                    User
-                </Nav.Link>
-              </Nav.Item>
-              <Nav.Item>
-                <Nav.Link 
-                  href="#sponsor"
-                  onClick={() => setRegisterSheet('sponsor')}>
-                    Sponsor
-                </Nav.Link>
-              </Nav.Item>
-            </Nav>
-          </Card.Header>
-          
-          <Card.Body>
-            {registerSheet === 'driver' ? <NewDriverForm /> : <NewSponsorForm />}
-          </Card.Body>
-        </Card>
-      </div>
-      <Footer />
-    </>
+    <div className="register">
+      <h1 className="register__heading">Register</h1>
+      <Card>
+        <Card.Header>
+          <Nav fill variant="tabs" defaultActiveKey="#driver">
+            <Nav.Item>
+              <Nav.Link
+                href="#driver"
+                onClick={() => setRegisterSheet('driver')}>
+                  User
+              </Nav.Link>
+            </Nav.Item>
+            <Nav.Item>
+              <Nav.Link 
+                href="#sponsor"
+                onClick={() => setRegisterSheet('sponsor')}>
+                  Sponsor
+              </Nav.Link>
+            </Nav.Item>
+          </Nav>
+        </Card.Header>
+        
+        <Card.Body>
+          {registerSheet === 'driver' ? <NewDriverForm /> : <NewSponsorForm />}
+        </Card.Body>
+      </Card>
+    </div>
   )
 }
 
