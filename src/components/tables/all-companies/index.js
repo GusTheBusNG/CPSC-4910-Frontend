98952444972ac71e80eaa01a9e0a0277def1c988
Nicholas Gustafson <nichgus7@gmail.com>
2020-02-20 15:47:03 -0500

    Adding the ability to create new company from the admin panel

--- src/components/tables/all-companies/index.js
+++ src/components/tables/all-companies/index.js
@@ -1,8 +1,10 @@
-import React from 'react';
+import React, { useState } from 'react';
 import Table from '../table';
 import { useQuery, useMutation } from '@apollo/react-hooks';
 import { getAllCompanies } from '../../../state/queries';
 import { deleteCompany, updateCompany } from '../../../state/mutations';
+import AddBox from '@material-ui/icons/AddBox';
+import NewSponsorForm from '../../new-sponsor-form';
 
 const AllDrivers = (props) => {
   const [deleteCompanyAction] = useMutation(deleteCompany, {
@@ -19,25 +21,38 @@ const AllDrivers = (props) => {
 
   const { loading, error, data } = useQuery(getAllCompanies);
 
+  const [showNewCompanyForm, setShowNewCompanyForm] = useState(false)
+
   if (loading) return <p>Loading...</p>;
   if (error) return <p>Error</p>;
 
   return (
-    <Table
-      columns={[
-        { title: "ID", field: "id", type: "numeric", editable: "never" },
-        { title: "Company Name", field: "name" },
-        { title: "Point to Dollar Ratio", field: "pointToDollarRatio" },
-        { title: "Description", field: "description" },
-      ]}
-      data={data.Companies}
-      title="Companies"
-      editable={{
-        onRowUpdate: ({ id, name, pointToDollarRatio, description }) => updateCompanyAction({ variables: { id, name, pointToDollarRatio, description }}),
-        onRowDelete: ({ id }) => deleteCompanyAction({ variables: { id }})
-      }}
-      {...props}
-    />
+    <>
+      <Table
+        columns={[
+          { title: "ID", field: "id", type: "numeric", editable: "never" },
+          { title: "Company Name", field: "name" },
+          { title: "Point to Dollar Ratio", field: "pointToDollarRatio" },
+          { title: "Description", field: "description" },
+        ]}
+        data={data.Companies}
+        title="Companies"
+        actions={[
+          {
+            icon: () => <AddBox />,
+            tooltip: 'Add User',
+            isFreeAction: true,
+            onClick: (event) => setShowNewCompanyForm(!showNewCompanyForm)
+          }
+        ]}
+        editable={{
+          onRowUpdate: ({ id, name, pointToDollarRatio, description }) => updateCompanyAction({ variables: { id, name, pointToDollarRatio, description }}),
+          onRowDelete: ({ id }) => deleteCompanyAction({ variables: { id }})
+        }}
+        {...props}
+      />
+      { showNewCompanyForm ? <NewSponsorForm /> : null }
+    </>
   );
 }
 
