2d30d20ca0b390fc6c3e1d6362f598d391255353
Nicholas Gustafson <nichgus7@gmail.com>
2020-02-23 15:05:32 -0500

    Removing extra tables component

--- src/components/driverpanel/index.js
+++ src/components/driverpanel/index.js
@@ -1,6 +1,6 @@
 import Button from '@material-ui/core/Button';
 import React from 'react';
-import Table from '../table';
+import Table from '../tables/table';
 
 import {fetchCompanies} from '../../state/queries';
 import { useQuery } from '@apollo/react-hooks';
